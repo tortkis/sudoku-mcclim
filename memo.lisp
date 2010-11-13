5de1f3d6eed70968197e1a64de99fa35874d49c8
Toru Takaishi <torutakaishi@Toru-Takaishis-MacBook-Air.local>
2010-11-13 09:37:11 -0800

    - removed an unnecessary file.

--- memo.lisp
+++
@@ -1,8 +0,0 @@
-
-
-;; create executable binary
-(sb-ext:save-lisp-and-die "/Users/torutakaishi/sudoku"
-                          :executable t
-                          :toplevel (lambda ()
-                                      (sudoku-mcclim::run)
-                                      (sb-ext:quit :unix-status 0)))
